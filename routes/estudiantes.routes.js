abd9995468197745c4b3e3bfab6309fd04c31044
Adolfo <vasquezadolfo482@gmail.com>
2024-08-17 15:34:08 -0400

    Get de estudiantes

--- routes/estudiantes.routes.js
+++ routes/estudiantes.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Estudiantes = require('../controllers/estudiantes')
 
-/* GET para Estudiantes */
+/* GET para los Estudiantes */
 router.get('/', function (req, res, next) {
   Estudiantes.listar()
     .then((resultado) => {
